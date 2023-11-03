354dd86e796dd5a640f31a4eb14130451e456038
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-03 22:46:17 +0100

    Add ffuse_mkdir callback function

--- include/callbacks.h
+++ include/callbacks.h
@@ -7,3 +7,4 @@ int ffuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t off
 int ffuse_create(const char *path, mode_t mode, struct fuse_file_info *fi);
 int ffuse_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
 int ffuse_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
+int ffuse_mkdir(const char *path, mode_t mode);
---
+++ src/callbacks/mkdir.c
@@ -0,0 +1,27 @@
+#include "tree.h"
+#include "ffuse.h"
+
+//* MKDIR ___________________________________________________________________*/
+int ffuse_mkdir(const char *path, mode_t mode)
+{
+    lock_tree();
+
+    // Create new file node
+    node_t *file = sys_add_file(path);
+    if (!file)
+        RETURN_UNLOCK_TREE(-ENOENT);
+
+    // Set file attributes
+    file->stat.st_mode = S_IFDIR | mode;
+    file->stat.st_nlink = 1;
+    file->stat.st_size = 0;
+    file->stat.st_uid = getuid();
+    file->stat.st_gid = getgid();
+    file->stat.st_atime = time(NULL);
+    file->stat.st_mtime = time(NULL);
+    file->stat.st_ctime = time(NULL);
+
+    print_tree();
+    unlock_tree();
+    return 0;
+}
