5ece51f1f6c69ea2a1cb80328da8e71f1e9305d9
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-03 21:12:59 +0100

    Add ffuse_create function to create new file nodes

--- src/main.c
+++ src/main.c
@@ -2,13 +2,38 @@
 #include "ffuse.h"
 #include "callbacks.h"
 
+static int ffuse_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+    // Check if file already exists
+    if (get_node(path) != NULL)
+        return -EEXIST;
+
+    // Create new file node
+    node_t *file = sys_add_file(path);
+    if (!file)
+        return -ENOENT;
+
+    // Set file attributes
+    file->stat.st_mode = S_IFREG | mode;
+    file->stat.st_nlink = 1;
+    file->stat.st_size = 0;
+    file->stat.st_uid = getuid();
+    file->stat.st_gid = getgid();
+    file->stat.st_atime = time(NULL);
+    file->stat.st_mtime = time(NULL);
+    file->stat.st_ctime = time(NULL);
+
+    return 0;
+}
+
+
 static struct fuse_operations ffuse_oper = {
     .getattr = ffuse_getattr,
     .readdir = ffuse_readdir,
     // .open = ffuse_open,
     // .read = ffuse_read,
     // .write = ffuse_write,
-    // .create = ffuse_create,
+    .create = ffuse_create,
     // .unlink = ffuse_unlink,
     // .mkdir = ffuse_mkdir,
     // .rmdir = ffuse_rmdir,
