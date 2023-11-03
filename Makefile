a2228892d779769cd893d1dd5e20cd80234d9ec8
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-03 20:58:25 +0100

    Add run_tests and myfuse to .gitignore, update Makefile for unit testing, and add initial unit tests for tree.c

--- Makefile
+++ Makefile
@@ -2,10 +2,17 @@ CC = gcc
 CFLAGS = -Wall -g3 $(shell pkg-config fuse3 --cflags) -Iinclude
 LDFLAGS = $(shell pkg-config fuse3 --libs)
 
+CHECK_CFLAGS = $(shell pkg-config --cflags check)
+CHECK_LDFLAGS = $(shell pkg-config --libs check)
+
 SRCDIR = src
+TESTDIR = tests
 SOURCES = $(shell find $(SRCDIR) -name '*.c')
 OBJECTS = $(SOURCES:.c=.o)
+TEST_SOURCES = $(shell find $(TESTDIR) -name '*_test.c')
+TEST_OBJECTS = $(TEST_SOURCES:.c=.o)
 EXECUTABLE = myfuse
+TEST_EXECUTABLE = run_tests
 
 all: $(EXECUTABLE)
 
@@ -15,7 +22,19 @@ $(EXECUTABLE): $(OBJECTS)
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
-clean:
-	rm -f $(OBJECTS) $(EXECUTABLE)
+$(TESTDIR)/%.o: $(TESTDIR)/%.c
+	$(CC) $(CFLAGS) $(CHECK_CFLAGS) -c $< -o $@
 
-re : clean all
+$(TEST_EXECUTABLE): $(TEST_OBJECTS) $(filter-out $(SRCDIR)/main.o, $(OBJECTS))
+	$(CC) $^ -o $@ $(CHECK_LDFLAGS) $(LDFLAGS)
+
+test: $(TEST_EXECUTABLE)
+	./$(TEST_EXECUTABLE)
+	@echo "Tests completed."
+
+clean:
+	rm -f $(OBJECTS) $(EXECUTABLE) $(TEST_OBJECTS) $(TEST_EXECUTABLE)
+
+re: clean all
+
+.PHONY: all clean re run_tests
---
+++ tests/tree_test.c
@@ -0,0 +1,117 @@
+#include <check.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "tree.h"
+
+//* SETUP ______________________________________________________________________*/
+void setup(void) {
+    init_root();
+}
+
+void teardown(void) {
+    free_tree(sys_get_root());
+}
+
+
+
+//* ADD FILE FROM ROOT _________________________________________________________*/
+START_TEST(test_sys_add_file) {
+    node_t *root = sys_get_root();
+    node_t *file = sys_add_file("/testfile");
+
+    ck_assert_ptr_nonnull(file);
+    ck_assert_ptr_eq(root->childs[0], file);
+}
+END_TEST
+
+//* ADD DIR FROM ROOT __________________________________________________________*/
+START_TEST(test_sys_add_dir) {
+    node_t *root = sys_get_root();
+    node_t *dir = sys_add_file("/testdir");
+
+    ck_assert_ptr_nonnull(dir);
+    ck_assert_ptr_eq(root->childs[0], dir);
+}
+
+//* ADD FILE FROM DIR __________________________________________________________*/
+START_TEST(test_sys_add_file_from_dir) {
+    node_t *dir = sys_add_file("/testdir");
+    node_t *file = sys_add_file("/testdir/testfile");
+
+    ck_assert_ptr_nonnull(file);
+    ck_assert_ptr_eq(dir->childs[0], file);
+}
+
+//* ADD DIR FROM DIR ___________________________________________________________*/
+START_TEST(test_sys_add_dir_from_dir) {
+    node_t *dir = sys_add_file("/testdir");
+    node_t *subdir = sys_add_file("/testdir/testsubdir");
+
+    ck_assert_ptr_nonnull(subdir);
+    ck_assert_ptr_eq(dir->childs[0], subdir);
+}
+
+//* LOTS OF FILES ______________________________________________________________*/
+START_TEST(test_sys_add_lots_of_files) {
+    node_t *dir = sys_add_file("/testdir");
+    node_t *file;
+    char path[100];
+
+    for (int i = 0; i < MAX_CHILD; i++) {
+        sprintf(path, "/testdir/testfile%d", i);
+        file = sys_add_file(path);
+        ck_assert_ptr_nonnull(file);
+        ck_assert_ptr_eq(dir->childs[i], file);
+    }
+}
+
+//* LOTS OF DIRS _______________________________________________________________*/
+START_TEST(test_sys_add_lots_of_dirs) {
+    node_t *dir = sys_add_file("/testdir");
+    node_t *subdir;
+    char path[100];
+
+    for (int i = 0; i < MAX_CHILD; i++) {
+        sprintf(path, "/testdir/testsubdir%d", i);
+        subdir = sys_add_file(path);
+        ck_assert_ptr_nonnull(subdir);
+        ck_assert_ptr_eq(dir->childs[i], subdir);
+    }
+}
+
+//* 5 LAYERS OF DIRS + 1 FILE IN EACH __________________________________________*/
+START_TEST(five_layers_of_dirs) {
+    node_t *dir = sys_add_file("/testdir");
+    node_t *subdir;
+    char path[100];
+
+    for (int i = 0; i < MAX_CHILD; i++) {
+        sprintf(path, "/testdir/testsubdir%d", i);
+        subdir = sys_add_file(path);
+        ck_assert_ptr_nonnull(subdir);
+        ck_assert_ptr_eq(dir->childs[i], subdir);
+    }
+}
+
+//* TREE SUITE __________________________________________________________________*/
+Suite * tree_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Tree");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_checked_fixture(tc_core, setup, teardown);
+    tcase_add_test(tc_core, test_sys_add_file);
+    tcase_add_test(tc_core, test_sys_add_dir);
+    tcase_add_test(tc_core, test_sys_add_file_from_dir);
+    tcase_add_test(tc_core, test_sys_add_dir_from_dir);
+    tcase_add_test(tc_core, test_sys_add_lots_of_files);
+    tcase_add_test(tc_core, test_sys_add_lots_of_dirs);
+    tcase_add_test(tc_core, five_layers_of_dirs);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
