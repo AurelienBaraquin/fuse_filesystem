96d5d13332b0c19fa008ff2d8ea92efc64767895
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-04 21:56:44 +0100

    Fix file extension and initialize new memory to zero when extending file size.

--- src/callbacks/write.c
+++ src/callbacks/write.c
@@ -11,10 +11,16 @@ int ffuse_write(const char *path, const char *buf, size_t size, off_t offset, st
         RETURN_UNLOCK_TREE(-ENOENT);
     }
     if (offset + size > node->stat.st_size) {
-        node->content = realloc(node->content, offset + size);
-        if (node->content == NULL) {
+        char *new_content = realloc(node->content, offset + size);
+        if (new_content == NULL) {
             RETURN_UNLOCK_TREE(-ENOMEM);
         }
+        // Initialize new memory to zero if the file is extended.
+        if (offset > node->stat.st_size) {
+            memset(new_content + node->stat.st_size, 0, offset - node->stat.st_size);
+        }
+        memset(new_content + offset, 0, size);
+        node->content = new_content;
         node->stat.st_size = offset + size;
     }
     memcpy(node->content + offset, buf, size);
