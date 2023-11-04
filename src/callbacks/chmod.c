9b2462e99deb6d1d8d00cce914180edcefed313e
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-04 20:12:19 +0100

    Update file permissions and modification time.

--- src/callbacks/chmod.c
+++ src/callbacks/chmod.c
@@ -6,9 +6,10 @@ int ffuse_chmod(const char *path, mode_t mode, struct fuse_file_info *fi)
 {
     lock_tree();
     (void) fi;
-    struct node *node = get_file(path);
-    if (node == NULL)
+    struct node *file = get_file(path);
+    if (file == NULL)
         RETURN_UNLOCK_TREE(-ENOENT);
-    node->stat.st_mode = mode;
+    file->stat.st_mode = mode;
+    file->stat.st_ctime = time(NULL);
     RETURN_UNLOCK_TREE(0);
 }
