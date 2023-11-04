2311258a6bedbc9361d5514a4e456a4ff47a3d26
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-04 22:01:08 +0100

    Fix null pointer and negative size check in ffuse_read function

--- src/callbacks/read.c
+++ src/callbacks/read.c
@@ -2,22 +2,29 @@
 #include "ffuse.h"
 
 //* READ ___________________________________________________________________*/
-int ffuse_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
-{
+int ffuse_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    if (buf == NULL || size < 0) {
+        return -EINVAL;
+    }
+
     lock_tree();
-    (void) fi;
+    
     struct node *file = get_file(path);
     if (file == NULL) {
         RETURN_UNLOCK_TREE(-ENOENT);
     }
+
     if (offset >= file->stat.st_size) {
         RETURN_UNLOCK_TREE(0);
     }
+    
     size_t bytes_to_read = size;
     if (offset + size > file->stat.st_size) {
         bytes_to_read = file->stat.st_size - offset;
     }
+
     memcpy(buf, file->content + offset, bytes_to_read);
     file->stat.st_atime = time(NULL);
+
     RETURN_UNLOCK_TREE(bytes_to_read);
 }
