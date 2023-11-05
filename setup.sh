dd47399f516d7a800259ed18b3ded75c2335b4e8
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-05 11:15:11 +0100

    build instructions to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Installing dependencies..."
+
 sudo apt-get update
 sudo apt-get install -y libfuse-dev check make gcc perl cpanminus libfuse3-dev zlib1g-dev
 cpanm --installdeps .
+
+echo "Done."
+
+echo "You can now build the project with 'make'."
