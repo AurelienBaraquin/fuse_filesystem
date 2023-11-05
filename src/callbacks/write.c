a2d904074f69b3eefcf43ae195e4460ca766d829
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-05 15:45:31 +0100

    Fix write, make the logic become more simple, just alloc and cpy memory into content

--- src/callbacks/write.c
+++ src/callbacks/write.c
@@ -1,6 +1,7 @@
 #include "tree.h"
 #include "ffuse.h"
 #include "compressor.h"
+#include "fd.h"
 
 //* WRITE ___________________________________________________________________*/
 int ffuse_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
@@ -11,20 +12,18 @@ int ffuse_write(const char *path, const char *buf, size_t size, off_t offset, st
     if (node == NULL) {
         RETURN_UNLOCK_TREE(-ENOENT);
     }
-    if (offset + size > node->stat.st_size) {
-        unsigned char *new_content = realloc(node->content, offset + size);
-        if (new_content == NULL) {
-            RETURN_UNLOCK_TREE(-ENOMEM);
-        }
-        // Initialize new memory to zero if the file is extended.
-        if (offset > node->stat.st_size) {
-            memset(new_content + node->stat.st_size, 0, offset - node->stat.st_size);
-        }
-        memset(new_content + offset, 0, size);
-        node->content = new_content;
-        node->stat.st_size = offset + size;
+
+    if (!S_ISREG(node->stat.st_mode)) {
+        RETURN_UNLOCK_TREE(-EISDIR);
     }
+
+    node->content = realloc(node->content, offset + size);
+    if (node->content == NULL) {
+        RETURN_UNLOCK_TREE(-ENOMEM);
+    }
+
     memcpy(node->content + offset, buf, size);
+    node->stat.st_size = offset + size;
     node->stat.st_mtime = time(NULL);
 
     compress_content(node);
